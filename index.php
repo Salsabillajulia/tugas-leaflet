801a7d9f16a9ca33a484143a8419b14d1b3c2bc5
1957051004 - Dina Putri Aulia <62508168+DinaPutriAulia@users.noreply.github.com>
2021-05-10 12:12:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,4 +9,20 @@
         }
         </style>
     <title>leaflet</title>
+    </head>
+    
+    <body>
+        <div id="mapid"></div>  
+    </body>
+    
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"integrity="sha512-
+    XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="crossorigin=""></script>
+    <script>
+    var mymap = L.map('mapid').setView([-5.4609937,105.3166303], 15);
+        
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a 
+        
+        
+    
    
